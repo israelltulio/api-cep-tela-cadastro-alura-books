6648243d053831271fd704db7a446e832300c076
Israell Túlio ® <121645979+israelltulio@users.noreply.github.com>
2023-03-02 23:32:17 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+async function buscaEndereco(cep) {
+    var mensagemErro = document.getElementById('erro');
+    mensagemErro.innerHTML = "";
+    try {
+        var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
+        var consultaCEPConvertida = await consultaCEP.json();
+        if (consultaCEPConvertida.erro) {
+            throw Error('CEP inexistente!');
+        }
+
+        var logradouro = document.getElementById('endereco');
+        var complemento = document.getElementById('complemento');
+        var bairro = document.getElementById('bairro');
+        var cidade = document.getElementById('cidade');
+        var estado = document.getElementById('estado');
+
+        logradouro.value = consultaCEPConvertida.logradouro;
+        complemento.value = consultaCEPConvertida.complemento;
+        bairro.value = consultaCEPConvertida.bairro;
+        cidade.value = consultaCEPConvertida.localidade;
+        estado.value = consultaCEPConvertida.uf;
+
+        console.log(consultaCEPConvertida);
+        return consultaCEPConvertida;
+    } catch (erro) {
+        mensagemErro.innerHTML = `<p>CEP Inválido. Tente novamente!</p>`
+        console.log(erro);
+    }
+}
+
+var cep = document.getElementById('cep');
+cep.addEventListener("focusout", () => buscaEndereco(cep.value));
